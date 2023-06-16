240f5d459a2cb790d99554806e469348219742f0
MASUDA TAKEHIRO <pgxt4184@gmail.com>
2023-06-16 21:20:16 +0900

    GitLearning.swiftを新規作成

---
+++ MyDietDiaryApp/Extension/Date+Extension.swift
@@ -0,0 +1,31 @@
+//
+//  Date+Extension.swift
+//  MyDietDiaryApp
+//
+//  Created by 増田剛大 on 2023/06/02.
+//
+
+import Foundation
+
+extension Date {
+    var calendar: Calendar {
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.timeZone = .current
+        calendar.locale = Locale(identifier: "ja-JP")
+        return calendar
+    }
+    
+    func fixed(year: Int? = nil, month: Int? = nil, day: Int? = nil, hour: Int? = nil, minute: Int? = nil, second: Int? = nil) -> Date {
+        var comp = DateComponents()
+        comp.year = year ?? calendar.component(.year, from: self)
+        comp.month = year ?? calendar.component(.month, from: self)
+        comp.day = year ?? calendar.component(.day, from: self)
+        comp.minute = year ?? calendar.component(.minute, from: self)
+        comp.second = year ?? calendar.component(.second, from: self)
+        return calendar.date(from: comp)!
+    }
+    
+    var zeroclock: Date {
+        return fixed(hour: 0, minute: 0, second: 0)
+    }
+}
